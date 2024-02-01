53fac5f83d819bf0a6b657e45b0cabdeb0baa108
Thiyaharasa Kukeenthan <138001135+TKukeenthan@users.noreply.github.com>
2024-02-01 22:42:52 +0530

    init commit

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiDart,
+  DiGit,
+  DiJava,
+
+  
+ 
+} from "react-icons/di";
+import {
+ 
+  SiFirebase,
+ 
+  SiSolidity,
+  SiPostgresql,
+  SiExpress,
+} from "react-icons/si";
+import { TbBrandFlutter } from "react-icons/tb";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbBrandFlutter />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSolidity />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDart />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import leaf from "../../Assets/Projects/leaf.png";
+import emotion from "../../Assets/Projects/emotion.png";
+import microfinace from "../../Assets/Projects/micro-finance-cover.jpg";
+import gld from "../../Assets/Projects/gld-cover.png";
+import lankaTimes from "../../Assets/Projects/lanka-time.jpg";
+import nexa from "../../Assets/Projects/nexa-erp.jpg";
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={gld}
+              isBlog={false}
+              title="Gas Leakage Detetor And Alerting System"
+              description="Innovating a state-of-the-art gas leakage detector and instant alert system, this project ensures swift notifications to individuals. The system boasts a user-friendly mobile app and an advanced IoT infrastructure, providing a seamless experience for timely responses and emergency coordination. "
+              subtitle="Tech:C ,Flutter,IoT,Firebase"
+              ghLink="https://github.com/TKukeenthan/Gas-Leakage-detector"
+             // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={microfinace}
+              isBlog={false}
+              title="Micro Finance"
+              description="Our Microfinance App is a robust and user-friendly platform designed to revolutionize the way microfinance institutions manage their operations and empower individuals with financial inclusion. With a focus on seamless role management and efficient loan management, this app is tailored to meet the diverse needs of microfinance organizations, ensuring a smoother and more accessible financial ecosystem. (On Going)"
+             subtitle="Tech:MangoDb,Express.js,ReactJs,NodeJs (MERN Stack)"
+             // ghLink="https://github.com/soumyajit4419/Plant_AI"
+              
+            //  demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={nexa}
+              isBlog={false}
+              title="NEXA ERP"
+              description="Revolutionize your business operations with the cutting-edge Nexa ERP mobile app, meticulously crafted for seamless efficiency and unparalleled productivity. Experience streamlined workflows and real-time insights at your fingertips, setting a new standard for business management. Available now on the Google Play Store for enhanced accessibility"
+             subtitle="Tech: Flutter, Laravel"
+              // ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              demoLink="https://play.google.com/store/apps/details?id=lk.bohar.NEXA_ERP"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={lankaTimes}
+              isBlog={false}
+              title="Lanka Times"
+              description="Innovatively conceived and launched the Lanka Time News mobile application, a cutting-edge platform now available on the Google Play Store (Link). Spearheaded the creation of a user-centric interface, ensuring seamless news consumption and real-time updates. Demonstrated expertise in mobile app development and strategic deployment for enhanced accessibility and engagement."
+             // ghLink="https://github.com/soumyajit4419/Editor.io"
+              demoLink="https://play.google.com/store/apps/details?id=lk.bohar.LankaTimes"              
+            />
+          </Col>
+
+         
+
+         
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
